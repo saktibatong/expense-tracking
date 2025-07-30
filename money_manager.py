5d95caf59664e73a21e9f6718cb3c3bfe969b707
Sakti Batong <saktibatong982@gmail.com>
2025-07-30 08:58:14 +0700

    add-account-summary

--- money_manager.py
+++ money_manager.py
@@ -1,112 +1,116 @@
 #%%
-import os
-import pandas as pd
-from datetime import datetime
 import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
-
-current_dir = os.getcwd()
-os.chdir(current_dir)
+import os
 
 #%%
-def accounts(account_df: str) -> list:
-    all_accounts = account_df['Accounts']
-    return all_accounts
-
-def income_categories():
-    return
-
-def input_transaction():
-    selected_acc = input('Acount name:')
-    transaction_datetime = datetime.now()
-    transaction_type = 'a'
-    payee = input('Transaction name:')
-    payment = input('Price:')
-    deposit = input('Payment received:')
-
-    transaction_list = [selected_acc, payee, transaction_datetime, payment, deposit]
-    return transaction_list
-
-def transaction_analysis(transaction_df, input):
-    return
-
-#%%
-accounts_df = pd.read_excel(r"./Database/Money Manager.xlsx", sheet_name='Accounts')
-accounts_df
-
-# %%
-transaction_df = pd.read_excel(r"./Database/Money Manager.xlsx", sheet_name='Transactions')
-transaction_df
-
-# %%
-accounts(accounts_df)
-
-# %%
-# Transaction calculation
-input_transaction()
-
-# %%
-st.title("Questionnaire")
-
-# Define questions
-questions = {
-    "How satisfied are you?": ["Very satisfied", "Satisfied", "Neutral", "Dissatisfied"],
-    "What is your favorite color?": ["Red", "Blue", "Green", "Other"],
-    "How often do you exercise?": ["Daily", "Weekly", "Rarely", "Never"]
-}
-
-# Store answers
-answers = {}
-
-for q, opts in questions.items():
-    answers[q] = st.selectbox(q, opts)
-
-# Button to submit
-if st.button("Submit"):
-    df = pd.DataFrame([answers])
-    st.write("Your answers:")
-    st.dataframe(df)
-    
-# %%
-
 # File to save the data
-DATA_FILE = "money_data.csv"
+transaction_file = "transaction.csv"
 
 # Load existing data
-if os.path.exists(DATA_FILE):
-    df = pd.read_csv(DATA_FILE)
+if os.path.exists(transaction_file):
+    df = pd.read_csv(transaction_file)
 else:
-    df = pd.DataFrame(columns=["Date", "Type", "Category", "Amount"])
+    df = pd.DataFrame(columns=["Date", "Payee", "Account", "Transaction category", "Income category", "Expense category", "Money received", "Payment"])
 
-st.title("💰 Money Tracker")
+#%%
+st.title("💰 Money Manager")
+
+st.subheader('📄 Daily Transaction')
+account_list = ['Checking BRI', 'Checking BCA', 'Checking JAGO', 'Checking Walet', 'Debt', 'Receivable', 'Saving JAGO', 'Saving Bibit']
+income_category = ['Dividends', 'Financial Aid', 'Gifts Received', 'Interest Income', 'Other Income', 'Refunds/Reimbursements', 'Teaching Income', 'Wages & Tips']
+expense_category = ['Alimony', 'Car Insurance', 'Car Payment', 'Car Repair / Licenses', 'Car Replacement Fund', 'Charity', 'Child Care', 'Cleaning', 'Clothing', 'Debt',
+                    'Dining', 'Discretionary', 'Doctor / Dentist', 'Education', 'Emergency Fund', 'E-Money', 'Family', 'Food & Drink', 'Fuel', 'Fun / Entertainment',
+                    'Furniture / Appliances', 'Gifts Given', 'Gopay', 'Groceries', 'Haircare', 'Health Insurance', 'Home Insurance', 'Home Supplies', 'Interest Expense', 'Life Insurance',
+                    'Medicine', 'Miscellaneous', 'Mortgage / Rent', 'Other Savings', 'OVO', 'Personal Supplies', 'Playing and Entertaining', 'Retirement Fund', 'Shopee Pay', 'Sport',
+                    'Subscriptions/Dues', 'Taxes', 'Transfer', 'Transportation', 'Util. Electricity', 'Util. Gas', 'Util. Phone(s)', 'Util. TV / Internet', 'Util. Water', 'Work Bills']
 
 # Input form
-with st.form("money_form"):
-    date = st.date_input("Date")
-    entry_type = st.selectbox("Type", ["Income", "Expense"])
-    category = st.selectbox("Category", ["Food", "Transport", "Salary", "Shopping", "Other"])
-    amount = st.number_input("Amount (IDR)", min_value=0.0, step=1000.0)
+date = st.date_input("Date")
+st.caption("Specific date on which a financial transaction occur")
 
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        new_data = pd.DataFrame([[date, entry_type, category, amount]], columns=df.columns)
+payee = st.text_input("Transaction name", placeholder="Enter the transaction name")
+st.caption("The recipient of funds, whether it's for a bill, a service, or any other type of financial transfer")
+
+account = st.selectbox("Account", account_list)
+st.caption("A record used to track financial transactions and manage financial activities")
+
+transaction_category = st.selectbox("Transaction category", ["Income", "Expense", "Transfer"])
+st.caption("The specific category of a financial transaction")
+
+# Conditional of transaction category
+selected_income = ""
+selected_expense = ""
+received_account = ""
+deposit = 0.00
+payment = 0.00
+if transaction_category == "Income":
+    selected_income = st.selectbox("Income category", income_category)
+    selected_expense = ""
+    deposit = st.number_input("Amount (IDR)", min_value=0.00, step=500.00, format="%.0f")
+    payment = 0.00
+
+elif transaction_category == "Expense":
+    selected_income = ""
+    selected_expense = st.selectbox("Expense category", expense_category)
+    deposit = 0.00
+    payment = st.number_input("Amount (IDR)", min_value=0.00, step=500.00, format="%.0f")
+
+else: # transaction category is transfer
+    received_account = st.selectbox("Received account", [i for i in account_list if i != account])
+    selected_income = "Transfer"
+    selected_expense = "Transfer"
+    payment = st.number_input("Amount (IDR)", min_value=0.0, step=500.00, format="%.0f")
+    deposit = payment
+
+# Dataframe analysis after submit a transaction
+if st.button("Submit"):
+    if transaction_category == "Income" or transaction_category == "Expense":
+        new_data = pd.DataFrame([[date, payee, account, transaction_category, selected_income, selected_expense, deposit, payment]], columns=df.columns)
         df = pd.concat([df, new_data], ignore_index=True)
-        df.to_csv(DATA_FILE, index=False)
-        st.success("Entry added!")
+        df.to_csv(transaction_file, index=False)
 
-# Show data
-st.subheader("📄 All Entries")
-st.dataframe(df)
+    else: # transaction category is transfer
+        new_data1 = pd.DataFrame([[date, payee, account, transaction_category, '', selected_expense, 0.00, payment]], columns=df.columns)
+        df = pd.concat([df, new_data1], ignore_index=True)
+        df.to_csv(transaction_file, index=False)
 
+        new_data2 = pd.DataFrame([[date, payee, received_account, transaction_category, selected_income, '', deposit, 0.00]], columns=df.columns)
+        df = pd.concat([df, new_data2], ignore_index=True)
+        df.to_csv(transaction_file, index=False)
+
+    st.success("Entry added!")
+
+    formatted_amount = f"{(deposit or payment):,.0f}".replace(",", ".")
+    st.write(f"📅 **{date}** — 💳 **{account}** — {transaction_category} of **Rp {formatted_amount}**")
+
+#%%
+# Show last 10 transactions
+st.subheader("📄 Last 10 transactions")
+last_transactional_df = df.copy()
+last_transactional_df['Sub-category'] = last_transactional_df['Income category'].fillna('') + last_transactional_df['Expense category'].fillna('')
+last_transactional_df['Amonut (IDR)'] = last_transactional_df['Money received'].fillna('') + last_transactional_df['Payment'].fillna('')
+last_transactional_df = last_transactional_df.drop(columns=['Income category', 'Expense category', 'Money received', 'Payment'])
+last_transactional_df = last_transactional_df.tail(10)  # Get last 10
+last_transactional_df.index = range(1, len(df) + 1)  # Set index from 1
+st.dataframe(last_transactional_df, hide_index=False)  # Show the index
+
+#%%
+# Account
+st.subheader("📄 Accounts net balance")
+income_expense_accounts = pd.pivot_table(df, index="Account", values=['Money received', 'Payment'], aggfunc='sum')
+income_expense_accounts.reset_index(inplace=True)
+income_expense_accounts['Net balance'] = income_expense_accounts['Money received'] - income_expense_accounts['Payment']
+
+account_total = income_expense_accounts[['Net balance']].sum()
+account_total['Account'] = 'Total'
+income_expense_total_acounts = pd.concat([income_expense_accounts, pd.DataFrame([account_total])], ignore_index=True)
+
+accounts_summary = income_expense_total_acounts[['Account', 'Net balance']]
+accounts_summary.columns = ['Account', 'Net balance (IDR)']
+st.dataframe(accounts_summary, hide_index=True)
+
+#%%
 # Analysis
-if not df.empty:
-    st.subheader("📊 Total Income and Expenses")
-    totals = df.groupby("Type")["Amount"].sum()
-    st.bar_chart(totals)
 
-    st.subheader("📂 Breakdown by Category")
-    category_totals = df[df["Type"] == "Expense"].groupby("Category")["Amount"].sum()
-    fig, ax = plt.subplots()
-    category_totals.plot.pie(autopct="%1.1f%%", ax=ax)
-    ax.set_ylabel("")
-    st.pyplot(fig)
